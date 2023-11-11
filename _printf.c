4e21f06fd1d73dabc6e97cded7a5cdf5d3a56adf
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-11 16:10:53 +0200

    printf file

--- _printf.c
+++ _printf.c
@@ -3,6 +3,64 @@
 #include "main.h"
 
 /**
+* print_char - Prints a character
+* @args: List of arguments
+* @count: Pointer to the count of characters
+*/
+void print_char(va_list args, int *count)
+{
+char c = va_arg(args, int);
+
+write(1, &c, 1);
+(*count)++;
+}
+
+/**
+* print_string - Prints a string
+* @args: List of arguments
+* @count: Pointer to the count of characters
+*/
+void print_string(va_list args, int *count)
+{
+char *s = va_arg(args, char *);
+
+for (int j = 0; s[j] != '\0'; j++)
+{
+write(1, &s[j], 1);
+(*count)++;
+}
+}
+
+/**
+* print_percent - Prints a percent symbol
+* @count: Pointer to the count of characters
+*/
+void print_percent(int *count)
+{
+write(1, "%", 1);
+(*count)++;
+}
+
+/**
+* print_int - Prints an integer
+* @args: List of arguments
+* @count: Pointer to the count of characters
+*/
+void print_int(va_list args, int *count)
+{
+int num = va_arg(args, int);
+char str[12]; /* Buffer big enough for an int. */
+
+sprintf(str, "%d", num); /* Convert the int to a string. */
+
+for (int j = 0; str[j] != '\0'; j++)
+{
+write(1, &str[j], 1);
+(*count)++;
+}
+}
+
+/**
 * _printf - a function that produces output according to a format
 * @format: character string (to be parsed)
 * Return: the number of characters printed (excluding the null byte)
@@ -26,44 +84,18 @@ i++;
 switch (format[i])
 {
 case 'c':
-{
-char c = va_arg(args, int);
-
-write(1, &c, 1);
-count++;
+print_char(args, &count);
 break;
-}
 case 's':
-{
-char *s = va_arg(args, char *);
-
-for (int j = 0; s[j] != '\0'; j++)
-{
-write(1, &s[j], 1);
-count++;
-}
+print_string(args, &count);
 break;
-}
 case '%':
-{
-write(1, &format[i], 1);
-count++;
+print_percent(&count);
 break;
-}
 case 'd':
 case 'i':
-{
-int num = va_arg(args, int);
-char str[12]; /* Buffer big enough for an int. */
-sprintf(str, "%d", num); /* Convert the int to a string. */
-
-for (int j = 0; str[j] != '\0'; j++)
-{
-write(1, &str[j], 1);
-count++;
-}
+print_int(args, &count);
 break;
-}
 default:
 break;
 }
--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+void print_char(va_list args, int *count);
+void print_string(va_list args, int *count);
+void print_percent(int *count);
+void print_int(va_list args, int *count);
 int _printf(const char *format, ...);
 
 #endif
