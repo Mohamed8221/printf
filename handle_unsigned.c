29a0b28f4193c941579d4c9a87b04fb81a425a7e
Mohamed8221 <s21511383@std.aou.edu.eg>
2023-11-12 13:12:17 +0200

    update

--- handle_unsigned.c
+++ handle_unsigned.c
@@ -1,6 +1,20 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+
+/**
+* print_number - prints an unsigned int
+* @n: number to print
+*
+* Description: This function prints an unsigned int.
+*/
+void print_number(int n)
+{
+if (n / 10)
+print_number(n / 10);
+putchar((n % 10) + '0');
+}
 
 /**
 * handle_unsigned_ - handle unsigned integers
@@ -13,7 +27,7 @@
 void handle_unsigned_(va_list args, int *count)
 {
 unsigned int n = va_arg(args, unsigned int);
-int temp = n;
+unsigned int temp = n;
 
 do {
 (*count)++;
